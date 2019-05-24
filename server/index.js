fa77908dc45712199a4e8f8b6ce790ca37d16775
スティーブンさん <underyourcover@icloud.com>
2019-05-23 21:44:57 -0700

    configured model && controller router logic

--- server/index.js
+++ server/index.js
@@ -1,24 +1,67 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const model = {
+  // HELPER FUNC to flatten nested JSON
+  flattenJSON: function(data) {
+    const result = {};
 
-const app = express();
+    function traverseJSON(cur, prop) {
+      if (Object(cur) !== cur) {
+        result[prop] = cur;
+      } else if (Array.isArray(cur)) {
+        let l;
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+        for (let i = 0, l = cur.length; i < l; i++)
+          traverseJSON(cur[i], prop + '[' + i + ']');
+        if (l === 0) result[prop] = [];
+      } else {
+        let isEmpty = true;
 
-app.use(express.static(path.join(__dirname, '../client')));
+        for (let p in cur) {
+          isEmpty = false;
+          traverseJSON(cur[p], prop ? prop + '.' + p : p);
+        }
+        if (isEmpty && prop) result[prop] = {};
+      }
+    }
 
-app.post('/convert', (req, res) => {
-  const formDataString = JSON.stringify(req.body.json);
-  res.send(formDataString);
-});
+    traverseJSON(data, '');
+    return result;
+  },
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`CSV Report Generator is listening on ${PORT}`);
-});
+  // HELPER FUNC to parse out CSV fields
+  parseCSVField: function(flatData, start = 0, end = 6) {
+    const csvFields = Object.keys(flatData);
+    return csvFields.slice(start, end).join();
+  },
+
+  // HELPER FUNC to parse out CSV rows
+  parseCSVData: function(flatData, chunk = 6) {
+    const csvData = Object.values(flatData);
+    let row;
+    const csvRows = [];
+
+    for (let i = 0; i < csvData.length; i += chunk) {
+      row = csvData.slice(i, i + chunk);
+      csvRows.push(row.join());
+    }
+    return csvRows.join('\n');
+  },
+
+  // HELPER FUNC to combine fieldsData + rowData
+  generateCSV: function(data) {
+    const flatData = this.flattenJSON(data);
+    const fieldsData = this.parseCSVField(flatData);
+    const rowData = this.parseCSVData(flatData);
+    return `${fieldsData}\n${rowData}`;
+  }
+};
+
+const controller = {
+  convertCSV: function(formDataString, callback) {
+    const csvData = model.generateCSV(formDataString);
+    callback(null, csvData);
+  }
+};
+
+module.exports = {
+  controller
+};
