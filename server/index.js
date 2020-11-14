7078c8befed52ad5ce0eb57e92a7e9e1d8428be6
Steven <steven@Stevens-MacBook-Pro.local>
2020-11-14 01:50:01 -0800

    change path

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+const routes = require('./routes/routes');
 
 const app = express();
 
